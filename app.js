2bcdd295f9d68174ff3b70cd3e68ea5868fe6d5c
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-12 00:05:45 +0200

    Add a color changing hover effect

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 
-const tag = document.querySelector(".container");
+const container = document.querySelector(".container");
 const size = 16;
 
 function grid() {
 
     for (let i=1; i<=size; i++) {
         for (let j=1; j<=size; j++) {
-            let div = document.createElement("div");
-            div.classList.add("test");
-            tag.appendChild(div);
+            let square = document.createElement("div");
+            square.classList.add("test");
+            container.appendChild(square);
         }
     }
+
+
 }
 
 grid();
+
+const test = document.getElementsByClassName("test");
+
+for (let i=0; i<test.length; i++) {
+    test[i].addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "orange";
+    }, false);
+}
