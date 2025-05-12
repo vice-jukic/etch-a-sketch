9a9668b1ff7d853a1266f3eba2e692bcb3266bac
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-12 21:06:57 +0200

    Add radnom color

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-const SCREEN_SIZE = "550px";
+const SCREEN_SIZE = 550;
 
 const container = document.querySelector(".container");
-container.style.width = SCREEN_SIZE;
-container.style.height = SCREEN_SIZE;
+container.style.width = SCREEN_SIZE + "px";
+container.style.height = SCREEN_SIZE + "px";
 
 let divNum = 16;
 
@@ -22,10 +22,20 @@ function createGrid() {
     return squares
 }
 
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    let color = `rgb(${r},${g},${b})`;
+
+    return color;
+}
+
 function changeColor(arr) {
     for (let i=0; i<arr.length; i++) {
         arr[i].addEventListener("mouseenter", (event) => {
-            event.target.style.backgroundColor = "orange";
+            event.target.style.backgroundColor = randomColor();
         }, false);
     }
 }
