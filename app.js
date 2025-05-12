1e60ea7e5ad45386f234e8e0fb8bbb3887889077
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-12 20:14:46 +0200

    Refactor code into more readable functions

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 
+const SCREEN_WIDTH = "550px";
+const SCREEN_HEIGHT = "550px";
+
 const container = document.querySelector(".container");
-const size = 16;
+container.style.width = SCREEN_WIDTH;
+container.style.height = SCREEN_HEIGHT;
 
-function grid() {
+let divNum = 16;
 
-    for (let i=1; i<=size; i++) {
-        for (let j=1; j<=size; j++) {
+function createGrid() {
+
+    for (let row=1; row<=divNum; row++) {
+        for (let col=1; col<=divNum; col++) {
             let square = document.createElement("div");
-            square.classList.add("test");
+            square.style.height = (550 / divNum) + "px";
+            square.style.width = (550 / divNum) + "px";
+            square.classList.add("square");
             container.appendChild(square);
         }
     }
-
-
+    const squares = document.getElementsByClassName("square");
+    return squares
 }
 
-grid();
-
-const test = document.getElementsByClassName("test");
-
-for (let i=0; i<test.length; i++) {
-    test[i].addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "orange";
-    }, false);
+function changeColor(arr) {
+    for (let i=0; i<arr.length; i++) {
+        arr[i].addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "orange";
+        }, false);
+    }
 }
+
+function run() {
+    const test = createGrid();
+    changeColor(test);
+}
+
+run();
